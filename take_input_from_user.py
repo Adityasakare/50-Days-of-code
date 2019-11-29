7bc25a0e001063a9522fad574db6f0bd84bd8c33
Adityasakare <46353199+Adityasakare@users.noreply.github.com>
2019-11-29 19:36:05 +0530

    Add files via upload

---
+++ take_input_from_user.py
@@ -0,0 +1,19 @@
+# input(" Enter a number: ")
+
+# x = 4+4
+
+# print(x)
+
+# x = input ("Enter a number: ")
+
+# print(x)
+
+#Assigment 2
+
+x = input(" Enter a number: ")
+
+y = input ("Whats your fav colour: ")
+
+print(x)
+
+print(y)
