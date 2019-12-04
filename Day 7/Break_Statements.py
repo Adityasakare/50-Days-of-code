6984336964fa579be57ed4fa8210e697bb469f5a
Adityasakare <46353199+Adityasakare@users.noreply.github.com>
2019-12-04 23:37:01 +0530

    Add files via upload

---
+++ Day 7/Break_Statements.py
@@ -0,0 +1,16 @@
+# List_Num = [20,10,30,45,5]
+
+# user = int(input("Enter a number"))
+
+# for i in List_Num:
+#     if i == user:
+#         print("We got a match")
+#         break
+# else:
+#     print("Sad life!!!!")
+
+
+for l in "Helloworld":
+    if l == 'o':
+        break
+    print(l)
---
+++ Day 7/Continue.py
@@ -0,0 +1,15 @@
+
+
+# for l in "Helloworld":
+#     if l == 'o':
+#         continue
+#     print(l)
+
+#PASS STATEMENT
+
+
+for l in "Helloworld":
+    if l == 'o':
+        pass
+        print("I will do it later")
+    print(l)
