76b47e5fad0c35311fad87208d7c2604e423936e
Aditya Sakare <aditya.sakare16@gmail.com>
2020-05-11 23:31:23 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,20 @@
+
+###INPUT PROGRAMS FUN 
+# ip = str(input("Enter your name: "))
+# print("Hello", ip.title())
+
+# num = int(input("Enter a number: "))
+
+# num+= 55
+
+# print(num)
+
+
+# num = int(input("Enter a number: "))
+
+
+# print(num*"\n Word") 
+
+
+result = eval(input("Enter an expression: "))
+print(result)
