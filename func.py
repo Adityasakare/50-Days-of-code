a3e225dbffca25b10cc4608295d63cccd2b08223
Aditya <Aditya.sakare16@gmail.com>
2020-05-20 19:36:19 +0530

    functions

---
+++ func.py
@@ -0,0 +1,17 @@
+# def myfunc():
+# 	print("I am aditya")
+
+# myfunc()
+
+#MULTIPLY
+def myfunc(a,b):
+	return a*b
+	# print(a*b)
+
+
+def hello():
+	print("Hello Aditya")
+
+
+
+aditya = 50
---
+++ func_2.py
@@ -0,0 +1,8 @@
+import func
+
+result = func.myfunc(6,6)
+print(result)
+
+func.hello()
+
+print(func.aditya)
